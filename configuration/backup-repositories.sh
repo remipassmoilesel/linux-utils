30a3908f750a1c8ad3d707129dcbbdc2fded7cce
Rémi Passmoilesel <remi.passmoilesel@gmail.com>
2017-10-16 11:23:24 +0200

    Remove direct usage of user name

--- configuration/backup-repositories.sh
+++ configuration/backup-repositories.sh
@@ -2,6 +2,6 @@
 
 echo "Start update: `date`" >> /git-rep/update.log
 
-sudo -u git bash -c "/home/remipassmoilesel/linux-utils/git-backup -b >> /git-rep/update.log"
+sudo -u git bash -c "/home/$USER/linux-utils/git-backup -b >> /git-rep/update.log"
 
 echo "Update done: `date`" >> /git-rep/update.log
