211b5a5832eb8680dd9219d4093c56aaea03f66b
Rémi Passmoilesel <remi.passmoilesel@gmail.com>
2018-01-31 23:34:06 +0100

    Improve notes

--- configuration/atom-packages.sh
+++ configuration/atom-packages.sh
@@ -2,6 +2,15 @@
 
 # jeudi 21 avril 2016, 22:21:11 (UTC+0200)
 
+# Helpers
+
+apm install minimap 
+
+# Typescript
+
+apm install atom-typescript atom-beautify linter-tslint 
+
+
 apm install atom-beautify project-manager minimap highlight-selected highlight-selected-minimap open-recent
 apm install minimap-highlight-selected linter linter-htmlhint linter-csslint linter-jshint autoprefixer
 apm install auto-detect-indentation color-picker file-icons javascript-snippets emmet
