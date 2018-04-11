87e061a32b177ca0da25507634614dba25c06ee6
Rémi Passmoilesel <remi.passmoilesel@gmail.com>
2018-04-11 18:20:15 +0200

    Improve custom rc, add hh plugin

---
+++ linux-config-helpers/install-hh.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# mercredi 11 avril 2018, 18:19:50 (UTC+0200)
+
+sudo add-apt-repository ppa:ultradvorka/ppa                                                                                                                                                       [18:19:06]  
+sudo apt-get update
+sudo apt-get install hh
