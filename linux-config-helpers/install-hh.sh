983d8596f6bc94ce5ae516a6fecfedb217ee570a
Rémi Passmoilesel <remi.passmoilesel@gmail.com>
2018-04-12 12:12:06 +0200

    Fix install script

--- linux-config-helpers/install-hh.sh
+++ linux-config-helpers/install-hh.sh
@@ -4,6 +4,6 @@
 
 # see https://github.com/dvorka/hstr/blob/master/README.md
 
-sudo add-apt-repository ppa:ultradvorka/ppa                                                                                                                                                       [18:19:06]  
+sudo add-apt-repository ppa:ultradvorka/ppa 
 sudo apt-get update
 sudo apt-get install hh
