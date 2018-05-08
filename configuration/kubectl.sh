a8a6b9245b0a265711898191d22bdbe287675674
Rémi Passmoilesel <remi.passmoilesel@gmail.com>
2018-05-08 14:14:15 +0200

    Improve notes, add helpers

---
+++ configuration/kubectl.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# mardi 8 mai 2018, 14:11:03 (UTC+0200)
+
+
+apt-get update && apt-get install -y apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb http://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+apt-get update
+apt-get install -y kubectl
