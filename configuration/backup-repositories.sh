dd1e07d4ef8449ce64cf73ecc7dacb655b4c0d3c
Rémi Passmoilesel <remi.passmoilesel@gmail.com>
2018-05-23 23:37:38 +0200

    Reorganize scripts

--- configuration/backup-repositories.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Start update: `date`" >> /git-rep/update.log
-
-sudo -u git bash -c "/home/$USER/linux-utils/git-backup -b >> /git-rep/update.log"
-
-echo "Update done: `date`" >> /git-rep/update.log
