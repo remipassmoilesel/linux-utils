06a35267612ef11ce14ffc01e09fc4d26273e991
Rémi Passmoilesel <remi.passmoilesel@gmail.com>
2018-06-08 13:56:13 +0200

    Improve cpu graph config

--- conky/conky_draw_config.lua
+++ conky/conky_draw_config.lua
@@ -2,16 +2,29 @@ function conky_get_cpu_graph_template()
     return {
         kind = 'ring_graph',
         conky_value = 'cpu cpu1',
-        center = { x = 140, y = 450 },
+        critical_threshold = 60,
+        --
+        center = { x = 140, y = 455 },
         radius = 20,
+        --
         background_color = 0xFFFFFF,
         background_alpha = 0.7,
         background_thickness = 2,
+        --
         bar_color = 0xFFFFFF,
         bar_alpha = 1,
         bar_thickness = 8,
+        --
         start_angle = 210,
-        end_angle = 360
+        end_angle = 360,
+        --
+        bar_color_critical = 0xFFA0A0,
+        bar_alpha_critical = 1,
+        bar_thickness_critical = 8,
+        --
+        background_color_critical = 0xFFA0A0,
+        background_alpha_critical = 0.7,
+        background_thickness_critical = 2,
     }
 end
 
