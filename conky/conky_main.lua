d1c37be1ca08f65d6b5ea6a44727fb96837fa12a
Rémi Passmoilesel <remi.passmoilesel@gmail.com>
2018-06-08 13:08:19 +0200

    Create function for import

--- conky/conky_main.lua
+++ conky/conky_main.lua
@@ -1,11 +1,16 @@
-local homeDirectory = os.getenv("HOME")
-local filesToImport = {
-    'conky_draw_config.lua',
-    'conky_helpers.lua',
-    'conky_draw.lua',
-}
 
-for index, fileName in ipairs(filesToImport) do
-    local fullPath = homeDirectory .. '/.conky/remipassmoilesel/' .. fileName
-    dofile(fullPath)
+function importAllFiles()
+    local homeDirectory = os.getenv("HOME")
+    local filesToImport = {
+        'conky_draw_config.lua',
+        'conky_helpers.lua',
+        'conky_draw.lua',
+    }
+
+    for index, fileName in ipairs(filesToImport) do
+        local fullPath = homeDirectory .. '/.conky/remipassmoilesel/' .. fileName
+        dofile(fullPath)
+    end
 end
+
+importAllFiles()
