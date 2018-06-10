d65ed58513fcda92a902dbb0288a834335f0655d
Rémi Passmoilesel <remi.passmoilesel@gmail.com>
2018-06-10 18:48:28 +0200

    Clean up config helpers

--- config-helpers/misc/scala.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# samedi 5 mai 2018, 19:41:12 (UTC+0200)
-
-echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
-sudo apt-get update
-sudo apt-get install sbt
