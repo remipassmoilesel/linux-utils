09449eea1bfcd03b7cfaea20cbc3255d0e1f8389
Rémi Passmoilesel <remi.passmoilesel@gmail.com>
2018-06-10 12:34:12 +0200

    Improve conky setup, add cache, rename vars

--- conky/conky_draw_templates.lua
+++ conky/conky_draw_templates.lua
@@ -1,3 +1,9 @@
+
+template_config = {
+    critical_color = 0xFFA0A0,
+    normal_color = 0xFFFFFF
+}
+
 function conky_get_cpu_graph_template()
     return {
         kind = 'ring_graph',
@@ -10,19 +16,19 @@ function conky_get_cpu_graph_template()
         start_angle = 210,
         end_angle = 360,
         --
-        bar_color = 0xFFFFFF,
+        bar_color = template_config.normal_color,
         bar_alpha = 0.9,
         bar_thickness = 8,
         --
-        background_color = 0xFFFFFF,
+        background_color = template_config.normal_color,
         background_alpha = 0.4,
         background_thickness = 2,
         --
-        bar_color_critical = 0xFFA0A0,
+        bar_color_critical = template_config.critical_color,
         bar_alpha_critical = 0.9,
         bar_thickness_critical = 8,
         --
-        background_color_critical = 0xFFA0A0,
+        background_color_critical = template_config.critical_color,
         background_alpha_critical = 0.4,
         background_thickness_critical = 2,
     }
@@ -34,18 +40,18 @@ function conky_get_bar_template()
         change_color_on_critical = true,
         --
         background_thickness = 8,
-        background_color = 0xFFFFFF,
+        background_color = template_config.normal_color,
         background_alpha = 0.3,
         --
-        bar_color = 0xFFFFFF,
+        bar_color = template_config.normal_color,
         bar_alpha = 0.9,
         bar_thickness = 8,
         --
-        bar_color_critical = 0xFFA0A0,
+        bar_color_critical = template_config.critical_color,
         bar_alpha_critical = 0.9,
         bar_thickness_critical = 8,
         --
-        background_color_critical = 0xFFA0A0,
+        background_color_critical = template_config.critical_color,
         background_alpha_critical = 0.3,
         background_thickness_critical = 8,
     }
--- conky/conky_helpers.lua
+++ conky/conky_helpers.lua
@@ -1,6 +1,7 @@
 
-custom_namespace = {
+network_cache = {
     public_address = nil,
+    private_addresses = nil,
 }
 
 function conky_command(command)
@@ -9,22 +10,25 @@ function conky_command(command)
 end
 
 function conky_public_address()
-    if (custom_namespace['public_address'] == nil) then
+    if (network_cache.public_address == nil) then
         local public_address = conky_command("curl ipinfo.io/ip"):sub(0, -2)
-        custom_namespace['public_address'] = public_address
+        network_cache.public_address = public_address
     end
-    return custom_namespace['public_address']
+    return network_cache.public_address
 end
 
 function conky_private_addresses()
-    local command = "ip a | grep -Eo 'inet (addr:)?([0-9]*\\.){3}[0-9]*' | grep -Eo '([0-9]*\\.){3}[0-9]*' | grep -v '127.0.0.1'"
-    local addressesStr = conky_command(command):sub(0, -2)
-    local addresses = string.gmatch(addressesStr, "[%d%p]+")
-    local filteredAddresses = {}
-    for addr in addresses do
-        if addr:sub(0, 3) ~= "172" then
-            table.insert(filteredAddresses, addr)
+    if (network_cache.private_addresses == nil) then
+        local command = "ip a | grep -Eo 'inet (addr:)?([0-9]*\\.){3}[0-9]*' | grep -Eo '([0-9]*\\.){3}[0-9]*' | grep -v '127.0.0.1'"
+        local addressesStr = conky_command(command):sub(0, -2)
+        local addresses = string.gmatch(addressesStr, "[%d%p]+")
+        local filteredAddresses = {}
+        for addr in addresses do
+            if addr:sub(0, 3) ~= "172" then
+                table.insert(filteredAddresses, addr)
+            end
         end
+        network_cache.private_addresses = table.concat(filteredAddresses, ', ')
     end
-    return table.concat(filteredAddresses, ', ')
+    return network_cache.private_addresses
 end
