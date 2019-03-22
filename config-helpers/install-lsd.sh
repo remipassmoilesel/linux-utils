d6c1df24f66896959fb94ac66676c57ecf579b07
Rémi Passmoilesel <r.passmoilesel@protonmail.com>
2019-03-23 00:17:51 +0100

    Add lsd

---
+++ config-helpers/install-lsd.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# samedi 23 mars 2019, 00:08:56 (UTC+0100)
+
+cd /tmp
+wget https://github.com/Peltoche/lsd/releases/download/0.14.0/lsd_0.14.0_amd64.deb
+sudo dpkg -i lsd_0.14.0_amd64.deb
+
