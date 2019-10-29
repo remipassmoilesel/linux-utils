754cb7740119033158dd90727af3f514f6a930e3
Rémi Passmoilesel <r.passmoilesel@protonmail.com>
2019-10-29 18:31:46 +0100

    Add first command line parsing

---
+++ memo-rs/src/argument_parser.rs
@@ -0,0 +1,59 @@
+use docopt::{Docopt, Error};
+use serde::Deserialize;
+
+use crate::argument_parser::MemoCommand::AddMemo;
+
+const USAGE: &'static str = "
+Memo 🚀 🚀 🚀
+
+Usage:
+  memo add <command> <description>
+  memo search <pattern>
+  memo edit
+  memo help
+
+Options:
+  -h --help     Show this screen.
+  --version     Show version.
+";
+
+#[derive(Debug, Deserialize)]
+struct CommandLineArgs {
+    cmd_add: bool,
+    arg_command: String,
+    arg_description: String,
+    arg_pattern: String,
+    cmd_edit: bool,
+    cmd_search: bool,
+    cmd_help: bool,
+}
+
+#[derive(Debug)]
+pub enum MemoCommand {
+    AddMemo { command: String, description: String },
+    Search { pattern: String },
+    Edit,
+}
+
+pub fn parse_arguments() -> Result<MemoCommand, String> {
+    let args: Result<CommandLineArgs, Error> = Docopt::new(USAGE)
+        .and_then(|d| d.deserialize());
+
+    match args {
+        Ok(args) => build_command(args),
+        Err(error) => Err(error.to_string())
+    }
+}
+
+fn build_command(args: CommandLineArgs) -> Result<MemoCommand, String> {
+    if (args.cmd_add) {
+        return Ok(MemoCommand::AddMemo { command: args.arg_command.clone(), description: args.arg_description.clone() });
+    }
+    if (args.cmd_search) {
+        return Ok(MemoCommand::Search { pattern: args.arg_pattern });
+    }
+    if (args.cmd_edit) {
+        return Ok(MemoCommand::Edit);
+    }
+    Err(String::from("Bad command"))
+}
--- memo-rs/src/main.rs
+++ memo-rs/src/main.rs
@@ -1,3 +1,12 @@
+
+mod argument_parser;
+use argument_parser::parse_arguments;
+
 fn main() {
     println!("Hello, world!");
+    let parse_result = parse_arguments();
+    match parse_result {
+        Ok(command) => println!("{:?}", command),
+        Err(e) => eprintln!("{:?}", e)
+    }
 }
