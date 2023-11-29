d9a5322a468fd9cd08b41678e1f7cfb3367d52a6
Rémi Passmoilesel <r.passmoilesel@protonmail.com>
2023-11-29 09:37:15 +0100

    Remove old stuff

--- configuration/helpers/misc/install-hh.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# mercredi 11 avril 2018, 18:19:50 (UTC+0200)
-
-# see https://github.com/dvorka/hstr/blob/master/README.md
-
-sudo add-apt-repository ppa:ultradvorka/ppa 
-sudo apt-get update
-sudo apt-get install hh
--- configuration/helpers/misc/install-lsd.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# samedi 23 mars 2019, 00:08:56 (UTC+0100)
-
-cd /tmp
-wget https://github.com/Peltoche/lsd/releases/download/0.14.0/lsd_0.14.0_amd64.deb
-sudo dpkg -i lsd_0.14.0_amd64.deb
-
